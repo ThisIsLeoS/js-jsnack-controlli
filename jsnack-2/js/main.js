1975c2493cd1560069fe7a76ba582b164983d4d4
leos <leonardo.siri.email@gmail.com>
2019-10-25 19:01:57 +0200

    change output string to a more informative one

--- jsnack-2/js/main.js
+++ jsnack-2/js/main.js
@@ -1,15 +1,15 @@
-/* L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga. */
+// L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 
 var firstWord = prompt("Enter the first word");
 var secondWord = prompt("Enter the second word");
 var longestWordMsg = document.querySelector(".longest-word-msg");
 if (firstWord.length < secondWord.length)
 {
-    longestWordMsg.textContent = firstWord + " " + secondWord;
+    longestWordMsg.textContent = "Words sorted by lenght: " + firstWord + ", " + secondWord;
 }
 else if (firstWord.length > secondWord.length)
 {
-    longestWordMsg.textContent = secondWord + " " + firstWord;
+    longestWordMsg.textContent = "Words sorted by lenght: " + secondWord + ", " + firstWord;
 }
 else
 {
